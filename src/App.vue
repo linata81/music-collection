e69302dd830615b8db192bb6f49a09b89282147e
Natalia Lysak <linata81@gmail.com>
2023-06-15 18:28:27 +0300

    add-to-favorite event

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,47 @@ import SongsPlayer from './components/SongsPlayer.vue'
 // const songs: Song[] = reactive([])
 const songs  = ref<Song[]>([])
 
+  const playingSongs: Song[] = [
+  {
+    id: '1',
+    title: 'So What',
+    artist: 'Miles Davis',
+    year: 1959,
+  },
+  {
+    id: '2',
+    title: 'Smells Like Teen Spirit',
+    artist: 'Nirvana',
+    year: 1991,
+  },
+  {
+    id: '3',
+    title: 'What a Wonderful World',
+    artist: 'Louis  Armstrong',
+    year: 1967,
+  },
+  {
+    id: '4',
+    title: 'The Look',
+    artist: 'Roxette',
+    year: 1988,
+  },
+]
+
 onMounted(async () => {
   getSongs(songs)
 })
+
+const addToFavourite = (songId: string) => {
+  console.log(songId);
+  
+}
 
 </script>
 
 <template>
   <main>
-    <SongsPlayer/>
+    <SongsPlayer :songs="playingSongs" @add-to-favorite="addToFavourite"/>
     <SongList :songs="songs"/>
   </main>
 </template>
--- src/components/SongsPlayer.vue
+++ src/components/SongsPlayer.vue
@@ -1,13 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Song } from '../types';
+
+const props = defineProps<{songs: Song[]}>()
+const emits = defineEmits<{(event: 'add-to-favorite', id:string):void}>()
+const currentSong = props.songs[Math.floor(Math.random() * props.songs.length)]
+</script>
 
 <template>
   <v-card title="Player" class="player">
     <v-card-text>
       <v-icon icon="mdi-play"></v-icon>
-      Song title
+      {{currentSong.artist}}. {{ currentSong.title }} ({{ currentSong.year  }})
     </v-card-text>
     <v-card-actions>
-      <v-btn>Add to favourites</v-btn>
+      <v-btn @click="emits('add-to-favorite', currentSong.id)">Add to favourites</v-btn>
     </v-card-actions>
   </v-card>
 </template>
